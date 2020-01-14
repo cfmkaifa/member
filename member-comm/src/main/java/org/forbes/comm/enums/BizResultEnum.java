37282b05059099891d8ded7aa583aac345362659
li3295552795 <tom.li@shdbr.com>
2020-01-14 09:44:36 +0800

    参数

--- member-comm/src/main/java/org/forbes/comm/enums/BizResultEnum.java
+++ member-comm/src/main/java/org/forbes/comm/enums/BizResultEnum.java
@@ -13,14 +13,15 @@ public enum BizResultEnum {
 	ENTITY_EMPTY("001001000","对象为空",""),
 
 	/*******会员*************/
-	NAME_EXISTS("003001001","姓名已存在","s%对应姓名已存在"),
+	USERNAME_EXISTS("003001001","用户已存在","s%对应用户已存在"),
 	USER_EMAIL_EXISTS("003001002","邮箱已存在","s%对应邮箱已存在"),
 	PHONE_EXISTS("003001003","电话已存在","s%对应电话已存在"),
 	FAX("003001004","邮箱已存在","s%对应邮箱已存在"),
 	ORG_CODE("003001005","机构代码已存在","s%对应机构代码已存在"),
 	MEMBER_EXIST("003001006","会员不存在","s%对应会员不存在"),
 	USER_EXIST("003001007","该用户名对应用户不存在","s%该用户名对应用户不存在"),
-    MEMBER_STATUS_NO_EXISTS("003001008","会员状态不存在","s%对应会员状态不存在"),
+
+	MEMBER_STATUS_NO_EXISTS("003001008","会员状态不存在","s%对应会员状态不存在"),
 
 	/*******地址(002)******/
 	ADRESS_STATUS_NO_EXISTS("003002001","地址状态不存在","%s对应地址状态不存在"),
@@ -30,7 +31,7 @@ public enum BizResultEnum {
 
 
 
-	
+
 	/**错误编码业务系统代码+功能编码+错误代码**/
 	private String bizCode;
 	/**错误描述****/
@@ -50,43 +51,43 @@ public enum BizResultEnum {
 		this.bizFormateMessage = bizFormateMessage;
 	}
 
-	/** 
-	 * @return bizCode 
+	/**
+	 * @return bizCode
 	 */
 	public String getBizCode() {
 		return bizCode;
 	}
 
-	/** 
-	 * @param bizCode 要设置的 bizCode 
+	/**
+	 * @param bizCode 要设置的 bizCode
 	 */
 	public void setBizCode(String bizCode) {
 		this.bizCode = bizCode;
 	}
 
-	/** 
-	 * @return bizMessage 
+	/**
+	 * @return bizMessage
 	 */
 	public String getBizMessage() {
 		return bizMessage;
 	}
 
-	/** 
-	 * @param bizMessage 要设置的 bizMessage 
+	/**
+	 * @param bizMessage 要设置的 bizMessage
 	 */
 	public void setBizMessage(String bizMessage) {
 		this.bizMessage = bizMessage;
 	}
 
-	/** 
-	 * @return bizFormateMessage 
+	/**
+	 * @return bizFormateMessage
 	 */
 	public String getBizFormateMessage() {
 		return bizFormateMessage;
 	}
 
-	/** 
-	 * @param bizFormateMessage 要设置的 bizFormateMessage 
+	/**
+	 * @param bizFormateMessage 要设置的 bizFormateMessage
 	 */
 	public void setBizFormateMessage(String bizFormateMessage) {
 		this.bizFormateMessage = bizFormateMessage;
